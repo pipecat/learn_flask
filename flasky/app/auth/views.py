22b49a86135c3ed841ed9947b335dcf67942623f
ubuntu <pipecat@yahoo.com>
2017-03-28 19:40:00 +0800

    finish login.html,base.html in auth

--- flasky/app/auth/views.py
+++ flasky/app/auth/views.py
@@ -1,6 +1,20 @@
-from flask import render_template
+from flask import render_template, url_for, redirect, flash, request
+from flask.ext.login import login_user
 from . import auth
+from ..models import User
+from .forms import LoginForm
 
 @auth.route('/')
 def login():
     return render_template('auth_login.html')
+
+@auth.route('/login', methods=['POST', 'GET'])
+def login():
+    form = LoginForm
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me,data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Invalid username or password')
+    return render_template('login.html', form=form)
