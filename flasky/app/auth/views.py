e25f5386eee39e373eb191ae965128b3255f18a0
ubuntu <pipecat@yahoo.com>
2017-04-02 21:31:06 +0800

    fix some bugs in sending email

--- flasky/app/auth/views.py
+++ flasky/app/auth/views.py
@@ -47,7 +47,7 @@ def before_request():
             and not current_user.confirmed \
             and request.endpoint[:5] != 'auth.' \
             and request.endpoint != 'static':
-        return redirect(url_for('auth.unconfirmed.html'))
+        return redirect(url_for('auth.unconfirmed'))
 
 @auth.route('/confirm/<token>')
 @login_required
@@ -72,4 +72,4 @@ def resend_confirmation():
     token = current_user.generate_confirmation_token()
     send_email(current_user.email, 'Confirm Your Account', 'auth/email/confirm', user=current_user, token=token)
     flash('A new confirmation email has been sent to you by email.')
-    return redirect(url_for('mian.index'))
+    return redirect(url_for('main.index'))
--- flasky/config.py
+++ flasky/config.py
@@ -1,4 +1,6 @@
 import os
+from email_user import mail_username, mail_password
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
@@ -6,8 +8,8 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     FLASKY_MAIL_SUBJECT_PREFIX = '[FLASKY]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <zry504557391@gmail.com>'
-    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    FLASKY_MAIL_SENDER = 'pipecat@qq.com'
+    FLASKY_ADMIN = 'pipecat@qq.com'
 
     @staticmethod
     def init_app(app):
@@ -16,11 +18,11 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_SERVER = 'smtp.qq.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_USERNAME = mail_username
+    MAIL_PASSWORD = mail_password
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URI') or \
         'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 
