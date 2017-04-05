cf929d9d38636f565f6543025e669f3666d12e60
ubuntu <pipecat@yahoo.com>
2017-04-05 21:09:10 +0800

    a bug version

--- flasky/app/auth/views.py
+++ flasky/app/auth/views.py
@@ -52,7 +52,7 @@ def before_request():
     '''
     if current_user.is_authenticated:
         current_user.ping()
-        if not current_user.unconfirmed \
+        if not current_user.confirmed \
                 and request.endpoint[:5] != 'auth':
             return redirect(url_for('auth.unconfirmed'))
 
