5d1a28266a12abbd49b3f5f359c97e419be84077
ubuntu <pipecat@yahoo.com>
2017-04-06 18:47:02 +0800

    add editor used by admin

--- flasky/app/main/forms.py
+++ flasky/app/main/forms.py
@@ -1,7 +1,9 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, BooleanField, TextField, SubmitField
-from wtforms.validators import Required, Length, Email
+from wtforms import StringField, BooleanField, TextField, SubmitField, SelectField, BooleanField
+from wtforms.validators import Required, Length, Email, Regexp
 from wtforms import ValidationError
+
+from ..models import Role, User
 
 class EditProfileForm(Form):
     """docstring for EditProfileForm"""
@@ -9,3 +11,32 @@ class EditProfileForm(Form):
     location = StringField('Location', validators=[Length(0,64)])
     about_me = TextField('About me')
     submit = SubmitField('Submit')
+
+class EditProfileAdminForm(Form):
+    email = StringField('Email', validators=[Required(), Length(0, 64), Email()])
+    username = StringField('Username', validators=[Required(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0, 'Username must have only letters, numbers, dots or underscores')])
+    confirmed = BooleanField('Confirmed')
+    role = SelectField('Role', coerce=int)
+    name = StringField('Real name', validators=[Length(0, 64)])
+    location = StringField('Location', validators=[Length(0, 64)])
+    about_me = TextField('About me')
+    submit = SubmitField('Submit')
+
+    def __init__(self, user, *args, **kwargs):
+        super(EditProfileAdminForm, self).__init__(*args, **kwargs)
+        self.role.choices = [(role.id, role.name) for role in Role.query.order_by(Role.name).all()]
+        self.user = user
+
+    def validate_email(self, field):
+        if field.data != self.user.email and \
+                User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered.')
+
+    def validate_username(self, field):
+        if field.data != self.user.username and \
+                User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username already in use.')
+
+
+
+
