58503ed134ad3492ce1c58f29ffe76b09fd4c7c9
ubuntu <pipecat@yahoo.com>
2017-04-06 19:25:02 +0800

    add surpport for gravatar

--- flasky/app/main/forms.py
+++ flasky/app/main/forms.py
@@ -36,7 +36,3 @@ class EditProfileAdminForm(Form):
         if field.data != self.user.username and \
                 User.query.filter_by(username=field.data).first():
             raise ValidationError('Username already in use.')
-
-
-
-
--- flasky/app/models.py
+++ flasky/app/models.py
@@ -1,8 +1,9 @@
-from flask import current_app
+from flask import current_app, request
 from flask.ext.login import UserMixin, AnonymousUserMixin
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
+import hashlib
 
 from . import db, login_manager
 
@@ -63,6 +64,7 @@ class User(UserMixin, db.Model):
     about_me = db.Column(db.Text())
     member_since = db.Column(db.DateTime(), default=datetime.utcnow)
     last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
+    avatar_hash = db.Column(db.String(32))
 
     def __init__(self,  **kwargs):
         super(User, self).__init__(**kwargs)
@@ -72,6 +74,8 @@ class User(UserMixin, db.Model):
             if self.role is None:
                 self.role = Role.query.filter_by(default=True).first()
         self.member_since = datetime.utcnow()
+        if self.email is not None and self.avatar_hash is None:
+            self.avatar_hash = hashlib.md5(self.email.encode('utf-8').hexdigest)
 
 
     def __repr__(self):
@@ -114,6 +118,14 @@ class User(UserMixin, db.Model):
     def ping(self):
         self.last_seen = datetime.utcnow()
         db.session.add(self)
+
+    def gravatar(self, size=100, default='identicon', rating='g'):
+        if request.is_secure:
+            url = 'https://secure.gravatar.com/avatar'
+        else:
+            url = 'http://www.gravatar.com/avatar'
+        hash = self.avatar_hash or hashlib.md5(self.email.encode('utf-8')).hexdigest()
+        return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(url=url, hash=hash, size=size, default=default, rating=rating)
 
 class AnonymousUer(AnonymousUserMixin):
     def can(self, permissions):
